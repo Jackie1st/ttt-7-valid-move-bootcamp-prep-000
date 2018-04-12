b29bdcdaa10bfd8fff2128ff6c0af0031f575626
Jackie First <jackiejeep@gmail.com>
2018-04-12 23:49:56 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,5 +12,6 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil 
     return false
-  else 
+  else if board[index] == "X" || board[index] == "O"
+    return true 
 end
